6a7e8a12ae676c28c9b8ad71ee17a150c4edb369
marvanessa1 <marvanessa1@live.com>
2022-04-29 03:43:07 -0700

    added prompt for initial user options, and updated schema and seed files

--- db/schema.sql
+++ db/schema.sql
@@ -4,26 +4,29 @@ CREATE DATABASE employees;
 USE employees;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-  id INT NOT NULL  AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL  AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
+  PRIMARY KEY (id),
   FOREIGN KEY (department_id)
   REFERENCES department(id)
+  ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT,
-  INDEX role_ind (role_id),
+  PRIMARY KEY (id),
   FOREIGN KEY (role_id) REFERENCES role(id),
   FOREIGN KEY (manager_id) REFERENCES employee(id)
   ON DELETE SET NULL
--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
 const db = require('./db/connection');
+const { listenerCount } = require('./db/connection');
+
 
 
 // Present user with options
+
+async function inquire(){
+    const answers = await inquirer 
+        .prompt([
+            {
+                type: "list",
+                name: "action",
+                message: "What would you like to do?",
+                choices: [
+                    {name: "View All Departments", value: 1},
+                    {name: "View All Roles", value: 2},
+                    {name: "View All Employess", value: 3},
+                    {name: "Add a department", value: 4},
+                    {name: "Add a role", value: 5},
+                    {name: "Add an employee", value: 6},
+                    {name: "Update an employee role", value: 7},
+                ]
+            }
+        ])
+        .then((answers) => {
+            answers.action();
+        });
+};
 
 // View all departments - READ - "SELECT * FROM [table_name]";
 async function viewAllDepartments(){
@@ -24,14 +50,77 @@ async function viewAllEmployees(){
 // Add a department - CREATE - "INSERT INTO [table_name] (col1, col2) VALUE (value1, value2)"
 
 // Add a role - CREATE - 
+async function createRole(){
+    // SELECT the existing department out for the 'departments' table
 
-    // SELECT the excisting roles out for the 'roles' table
+    const departments = [
+        {
+            id: 1,
+            name: "Executive"
+        },
+        {
+            id: 2,
+            name: "Transfiguration"
+        },
+        {
+            id: 3,
+            name: "Charms"
+        },
+        {
+            id: 4,
+            name: "Potions"
+        },
+        {
+            id: 5,
+            name: "History of Magic"
+        },
+        {
+            id: 6,
+            name: "Defence Against the Dark Arts"
+        },
+        {
+            id: 7,
+            name: "Astronomy"
+        },
+        {
+            id: 8,
+            name: "Herbology"
+        },
+        {
+            id: 9,
+            name: "Divination"
+        },
+        {
+            id: 10,
+            name: "Care of Maginal Creatures"
+        },
+    ]
 
-    // .map() the resulds from 'roles' to question data for inquirer
+      // .map() the resulds from 'department' to question data for inquirer
 
-    // THEN prompt the user for role information (inquirer)
+    const choices = department.map (department => {
+        return {
+            name: department.name,
+            value: department.id
+        }
+    })
+      
+    const answers = await inquirer
+    .prompt([
+       { 
+        type:"list",
+        name: "department_id",
+        messsage: "Select a department",
+        choices: choices
+        }
+    ])
+    .then((answers) =>{
+        console.log(answers);
+    })
+       // Take the user's answers and go INSERT them into the 'role table'
+}
 
-        // Take the user's answers and go INSERT them into the 'role table'
+
 
 // Add an employee - CREATE - 
 
